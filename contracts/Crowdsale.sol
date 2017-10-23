2892b611c213cc3056981badab54d3eeeaf7b2bd
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2017-10-22 23:11:29 -0400

    Safe math and crowdsale contract

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,47 +1,70 @@
 pragma solidity ^0.4.8;
 import "./StandardToken.sol";
+import "./SafeMath.sol";
+import "./Ownable.sol";
 
-contract Crowdsale {
-
+contract Crowdsale is Ownable {
+  using SafeMath for uint256;
   StandardToken public token;
   uint public startTime;
   uint public endTime;
-  uint public hardCap;
-  uint public softCap;
+  uint256 public hardCap;
+  uint256 public softCap;
   address public multiSig;
   address public beneficiary;
-  uint public weiRaisedSoFar;
+  uint public weiRaised;
+  mapping(address => uint256) public deposited;
 
 
 
   function Crowdsale (address _tokenAddress,
     uint _startTime,
     uint _endTime,
-    uint _softCap,
-    uint _hardCap,
+    uint256 _softCap,
+    uint256 _hardCap,
     address _beneficiary,
     address _multiSig) {
 
-    token = StandardToken(_tokenAddress);
     assert(_startTime > 0 && _startTime >=now);
     assert(_endTime > _startTime);
     assert(_hardCap > _softCap);
+    assert(_beneficiary != address(0));
+    assert(_multiSig != address(0));
+
+    token = StandardToken(_tokenAddress);
     startTime = _startTime;
     endTime = _endTime;
     hardCap = _hardCap;
     multiSig = _multiSig;
     beneficiary = _beneficiary;
-  }
-
-  function contribute() public {
 
   }
 
-  function refund() public {
 
+  function contribute() public payable {
+    assert(msg.value > 0);
+    uint256 value = msg.value;
+    uint256 tokenFor1ETH = 2000;
+    uint256 rate = tokenFor1ETH.div(10**18);
+    uint256 numTokens = rate.mul(value);
+    weiRaised = weiRaised.add(msg.value);
+    deposited[msg.sender] += msg.value;
+    require(token.transferFrom(beneficiary, msg.sender, numTokens));
+
+  }
+
+  function refund(address investor) public {
+    assert (deposited[investor] > 0);
+    uint256 depositedValue = deposited[investor];
+    deposited[investor] = 0;
+    investor.transfer(depositedValue);
   }
 
   function() payable {
     contribute();
+  }
+
+  function finalizeCrowdsale() onlyOwner {
+    multiSig.transfer(this.balance);
   }
 }
