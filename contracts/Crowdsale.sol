553357fe1114fcf367a52ea99ea2b63994009310
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2017-10-23 00:11:30 -0400

    Crowdsale contract improvements

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -10,7 +10,7 @@ contract Crowdsale is Ownable {
   uint public endTime;
   uint256 public hardCap;
   uint256 public softCap;
-  address public multiSig;
+  address public wallet;
   address public beneficiary;
   uint public weiRaised;
   mapping(address => uint256) public deposited;
@@ -23,29 +23,29 @@ contract Crowdsale is Ownable {
     uint256 _softCap,
     uint256 _hardCap,
     address _beneficiary,
-    address _multiSig) {
+    address _wallet) {
 
-    assert(_startTime > 0 && _startTime >=now);
-    assert(_endTime > _startTime);
-    assert(_hardCap > _softCap);
-    assert(_beneficiary != address(0));
-    assert(_multiSig != address(0));
+    require(_startTime > 0 && _startTime >=now);
+    require(_endTime > _startTime);
+    require(_hardCap > _softCap && _softCap > 0);
+    require(_beneficiary != address(0));
+    require(_wallet != address(0));
 
     token = StandardToken(_tokenAddress);
     startTime = _startTime;
     endTime = _endTime;
     hardCap = _hardCap;
-    multiSig = _multiSig;
+    wallet = _wallet;
     beneficiary = _beneficiary;
 
   }
 
 
-  function contribute() public payable {
-    assert(msg.value > 0);
+  function contribute()  public payable {
+    require(validPurchase());
     uint256 value = msg.value;
-    uint256 tokenFor1ETH = 2000;
-    uint256 rate = tokenFor1ETH.div(10**18);
+    uint256 tokenForOneETH = 2000;
+    uint256 rate = tokenForOneETH.div(10**18);
     uint256 numTokens = rate.mul(value);
     weiRaised = weiRaised.add(msg.value);
     deposited[msg.sender] += msg.value;
@@ -53,11 +53,19 @@ contract Crowdsale is Ownable {
 
   }
 
-  function refund(address investor) public {
-    assert (deposited[investor] > 0);
+  function refund() public {
+    require(hasEnded());
+    require(!goalReached());
+    address investor = msg.sender;
+    require(deposited[investor] > 0);
     uint256 depositedValue = deposited[investor];
     deposited[investor] = 0;
     investor.transfer(depositedValue);
+  }
+
+  function validPurchase() returns(bool) {
+    bool withinReached = weiRaised.add(msg.value) <= hardCap;
+    return !hasEnded() && !withinReached;
   }
 
   function() payable {
@@ -65,6 +73,19 @@ contract Crowdsale is Ownable {
   }
 
   function finalizeCrowdsale() onlyOwner {
-    multiSig.transfer(this.balance);
+    require(goalReached());
+    require(!hasEnded());
+    wallet.transfer(this.balance);
+  }
+
+  function hasEnded() public constant returns(bool) {
+    return now > hasEnded;
+  }
+
+  function hardCapReached() public constant returns(bool) {
+    return weiRaised >= hardCap;
+  }
+  function goalReached() public constant returns(bool) {
+    return weiRaised >= softCap;
   }
 }
