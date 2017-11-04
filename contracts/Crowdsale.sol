88796d27ca838eb27bd30eefaf2ef4283475adfb
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2017-11-04 16:55:13 -0400

    Make crowdsale pausable

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -2,8 +2,9 @@ pragma solidity ^0.4.8;
 import "./StandardToken.sol";
 import "./SafeMath.sol";
 import "./Ownable.sol";
+import "./Pausable.sol";
 
-contract Crowdsale is Ownable {
+contract Crowdsale is Ownable, Pausable {
   using SafeMath for uint256;
   StandardToken public token;
   uint256 public startTime;
@@ -14,9 +15,9 @@ contract Crowdsale is Ownable {
   address public beneficiary;
   uint256 public weiRaised;
   uint256 public TOKEN_FOR_ONE_ETH = 2000;
-  mapping(address => uint256) public deposited;
-  event tokensPurchased(address, uint, uint);
 
+  mapping(address => uint256) public deposited;
+  event TokensPurchased(address indexed _to, uint256 _tokens);
 
   function Crowdsale (address _tokenAddress,
     uint _startTime,
@@ -25,26 +26,21 @@ contract Crowdsale is Ownable {
     uint256 _hardCap,
     address _beneficiary,
     address _wallet) {
-
     require(_startTime >= now);
-    startTime = _startTime;
     require(duration > 0);
-    uint _endTime = _startTime + (duration * 1 days);
-
-
     require(_hardCap > _softCap && _softCap > 0);
     require(_beneficiary != address(0));
     require(_wallet != address(0));
 
     token = StandardToken(_tokenAddress);
     startTime = _startTime;
-    endTime = _endTime;
+    endTime = _startTime + (duration * 1 days);
     hardCap = _hardCap;
     wallet = _wallet;
     beneficiary = _beneficiary;
   }
 
-  function contribute()  public payable returns (uint256) {
+  function contribute()  whenNotPaused public payable returns (uint256) {
     uint256 value = msg.value;
     require(validPurchase(msg.value));
     uint256 numTokens = value.mul(TOKEN_FOR_ONE_ETH);
@@ -54,14 +50,13 @@ contract Crowdsale is Ownable {
     deposited[msg.sender] = currentContribution.add(msg.value);
     require(token.balanceOf(beneficiary) >= numTokens);
     require(token.transferFrom(beneficiary, msg.sender, numTokens));
-
+    TokensPurchased(msg.sender, numTokens);
     return numTokens;
   }
 
-  function refund() public {
+  function refund(address investor) onlyOwner whenNotPaused public {
     require(!inOperation());
     require(!goalReached());
-    address investor = msg.sender;
     require(deposited[investor] > 0);
     uint256 depositedValue = deposited[investor];
     deposited[investor] = 0;
@@ -77,7 +72,7 @@ contract Crowdsale is Ownable {
     contribute();
   }
 
-  function finalizeCrowdsale() onlyOwner {
+  function finalizeCrowdsale() whenNotPaused onlyOwner public {
     require(goalReached());
     require(!inOperation() || weiRaised >= hardCap);
     wallet.transfer(this.balance);
